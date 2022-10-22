631aafda7712c9ce8336019d2cde106fc4de35b1
Harshada <harshadajagtap199725@gmail.com>
2022-10-22 09:37:37 +0530

    furnitire data

--- mockdata.js
+++ mockdata.js
@@ -6,9 +6,11 @@ const faker = require("faker");
 // Create an object for config file
 const db = {
   //books:[],
-  dogs: [],
-  posts: [],
-  comments: [],
+  // dogs: [],
+  // posts: [],
+  // comments: [],
+  furniture: [],
+  wishlisted: [],
 };
 
 for (let i = 1; i <= 5; i++) {
@@ -22,32 +24,46 @@ for (let i = 1; i <= 5; i++) {
   // });
 
   // users:
-  db.dogs.push({
+  // db.dogs.push({
+  //   id: i,
+  //   name: casual.name,
+  //   age: casual.age,
+  //   place: casual.place,
+  //   gender: casual.gender,
+  // });
+  db.furniture.push({
     id: i,
-    name: casual.name,
-    age: casual.age,
-    place: casual.place,
-    gender: casual.gender,
+    type: casual.type,
+    year: casual.year,
+    Description: casual.Description,
+    price: casual.price,
+  });
+  db.wishlisted.push({
+    id: i,
+    type: casual.type,
+    year: casual.year,
+    Description: casual.Description,
+    price: casual.price,
   });
 
   // blog posts
-  db.posts.push({
-    id: i,
-    title: casual.title,
-    body: casual.sentences((n = casual.integer(10, 20))),
-    user_id: casual.integer(1, 10),
-    date: casual.date((format = "YYYY-MM-DD")),
-    images: [faker.random.image(), faker.random.image()],
-  });
+  // db.posts.push({
+  //   id: i,
+  //   title: casual.title,
+  //   body: casual.sentences((n = casual.integer(10, 20))),
+  //   user_id: casual.integer(1, 10),
+  //   date: casual.date((format = "YYYY-MM-DD")),
+  //   images: [faker.random.image(), faker.random.image()],
+  // });
 
-  // comments
-  for (let j = 1; j <= casual.integer(1, 5); j++) {
-    db.comments.push({
-      id: db.comments.length + 1,
-      post_id: i,
-      body: casual.sentences((n = casual.integer(2, 10))),
-      date: casual.date((format = "YYYY-MM-DD")),
-    });
-  }
+  // // comments
+  // for (let j = 1; j <= casual.integer(1, 5); j++) {
+  //   db.comments.push({
+  //     id: db.comments.length + 1,
+  //     post_id: i,
+  //     body: casual.sentences((n = casual.integer(2, 10))),
+  //     date: casual.date((format = "YYYY-MM-DD")),
+  //   });
+  // }
 }
 console.log(JSON.stringify(db));
